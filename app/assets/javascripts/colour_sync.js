590dda76f67b31edd9789913e45f60fdd3effffe
maxc-dev <mer.cartermc@gmail.com>
2020-10-25 22:04:12 +0000

    Added header/footer RGB sync

---
+++ app/assets/javascripts/colour_sync.js
@@ -0,0 +1,55 @@
+class ColouredNode {
+    constructor(element, id) {
+        this.element = element;
+        this.id = id;
+    }
+
+    getElement() {
+        return this.element
+    }
+
+    getId() {
+        return this.id
+    }
+}
+
+let nodes = []
+let rgb = [0, 128, 256]
+let up = [true, true, true]
+
+function addColourCycleNode(element, id) {
+    let node = new ColouredNode(element, id)
+    if (!nodes.includes(node)) {
+        nodes.push(node)
+    }
+    return node
+}
+
+
+function addColourHoverListener(targetNode) {
+    $(targetNode.getElement()).hover(
+        function cycle() {
+            for (let i = 0; i < rgb.length; i++) {
+                if (up[i]) {
+                    rgb[i]++;
+                } else {
+                    rgb[i]--;
+                }
+                if (rgb[i] >= 256) {
+                    up[i] = false
+                } else if (rgb[i] <= 0) {
+                    up[i] = true
+                }
+            }
+            for (let i = 0; i < nodes.length; i++) {
+                document.getElementById(nodes[i].getId()).style.backgroundColor = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")"
+            }
+            if ($(targetNode.getElement()).is(":hover")) {
+                setTimeout(function () {
+                    cycle()
+                }, 10);
+            }
+        }
+    )
+}
+
